290133241c92a90032b2b1c57b31178df30e490e
Charmi <c0768448@mylambton.ca>
2020-07-12 18:52:55 -0400

    add maps activity with fragment

---
+++ app/src/main/java/com/example/maps_charmi_c0768448/mapsActivity.java
@@ -0,0 +1,37 @@
+package com.example.maps_charmi_c0768448;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class mapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener
+{
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+}
